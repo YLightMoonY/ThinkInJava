6a3ac8a6f726420606183da6bd9975a063ffca6f
Sunjin <BrokeMoon@outlook.com>
2020-01-17 14:08:32 +0800

    update

---
+++ src/lmoon/chap15/GenericMethods.java
@@ -0,0 +1,14 @@
+package lmoon.chap15;
+
+public class GenericMethods {
+    public <T> void f(T x) {
+        System.out.println(x.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        GenericMethods gm = new GenericMethods();
+        gm.f("");
+        gm.f(1);
+        gm.f(1.0);
+    }
+}
